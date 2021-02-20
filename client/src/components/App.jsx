5bbaeb6eda4a64eb7803df0d0c2c0edf71c8dfaa
Adam Whitman <adam.whitmanpt@gmail.com>
2021-02-20 11:15:00 -0800

    Working on search bar functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import axios from 'axios';
+import MovieList from './ListOfMovies';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      currentListOfMovies: []
+    }
+
+    this.searchForMovie = this.searchForMovie.bind(this);
+    this.searchClickHandler = this.searchClickHandler.bind(this);
+  }
+
+  searchForMovie(event) {
+
+
+    this.setState({title: event.target.value})
+  }
+
+  searchClickHandler(event) {
+    event.preventDefault();
+
+    let targetMovie = currentListOfMovies;
+
+    targetMovie.push({title: event.target.value});
+
+    this.setState({currentListOfMovies: targetMovie});
+
+    this.setState({title: ''});
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Movie List</h1>
+        <form>
+          <label>
+            Find a Movie:
+            <input type='text' onChange={this.searchForMovie}/>
+          </label>
+          <input type='submit' value='Submit' onClick={this.searchClickHandler}/>
+        </form>
+        <MovieList />
+      </div>
+    )
+  }
+}
 
 export default App;
---
+++ client/src/components/ListOfMovies.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MovieList = (props) => {
+  return(
+    <ul>
+      <li>{'Mean Girls'}</li>
+      <li>{'Hackers'}</li>
+      <li>{'The Grey'}</li>
+      <li>{'Sunshine'}</li>
+      <li>{'Ex Machina'}</li>
+    </ul>
+  )
+}
+
+export default MovieList;
